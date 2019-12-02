5ab6453e1257b19fca8194977d61f085496ac552
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-12-02 10:58:43 -0600

    Added first GET request

--- src/main/java/com/midoes/vertxapp/MainVerticle.java
+++ src/main/java/com/midoes/vertxapp/MainVerticle.java
@@ -6,10 +6,12 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -31,6 +33,9 @@ public class MainVerticle extends AbstractVerticle {
   public void start() {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
+
+    router.get("/api/v1/test").handler(this::getTest);
+
     router.route().handler(routingContext -> {
       // This handler will be called for every request
       HttpServerResponse response = routingContext.response();
@@ -40,6 +45,15 @@ public class MainVerticle extends AbstractVerticle {
     });
     server.requestHandler(router).listen(config().getInteger("http.port"));
     LOGGER.info("Hello World API Verticle App started");
+  }
+
+  private void getTest(RoutingContext routingContext) {
+    JsonObject response = new JsonObject();
+    response.put("test", "Test method is working!");
+    routingContext.response()
+      .setStatusCode(200)
+      .putHeader("content-type", "application/json")
+      .end(Json.encodePrettily(response));
   }
 
   @Override
