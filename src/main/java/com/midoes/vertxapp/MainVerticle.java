6b9d879d313235cd50fd8cbcd16578939e02ccd7
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-12-09 11:20:01 -0600

    Added default routing and authorization token

--- src/main/java/com/midoes/vertxapp/MainVerticle.java
+++ src/main/java/com/midoes/vertxapp/MainVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
@@ -33,9 +34,8 @@ public class MainVerticle extends AbstractVerticle {
   public void start() {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
-
+    router.get("/api*").handler(this::defaultProcessorForAllAPI);
     router.get("/api/v1/test").handler(this::getTest);
-
     router.route().handler(routingContext -> {
       // This handler will be called for every request
       HttpServerResponse response = routingContext.response();
@@ -45,6 +45,26 @@ public class MainVerticle extends AbstractVerticle {
     });
     server.requestHandler(router).listen(config().getInteger("http.port"));
     LOGGER.info("Hello World API Verticle App started");
+  }
+
+  // Call for all default API HTTP GET, POST, PUT and DELETE
+  private void defaultProcessorForAllAPI(RoutingContext routingContext) {
+    String authToken = routingContext.request().headers().get("AuthToken");
+    if (authToken == null || !authToken.equals("123")) {
+      routingContext.response()
+        .setStatusCode(401)
+        .putHeader(HttpHeaders.CONTENT_TYPE, "content-type")
+        .end(Json.encodePrettily(new JsonObject().put("error", "Not Authorized to use this API")));
+      LOGGER.info("Failed basic auth check");
+    } else {
+      LOGGER.info("Passed basic auth check");
+      // Call the next matching route
+      routingContext.response()
+        .putHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+      routingContext.response()
+        .putHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE");
+      routingContext.next();
+    }
   }
 
   private void getTest(RoutingContext routingContext) {
